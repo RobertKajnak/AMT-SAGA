e9641d37b0131aa162a9765c5e47962ee8e51d56
RobertKajnak <hezirisz@gmail.com>
2019-06-08 19:49:43 +0200

    Revamped sliding window approach
    Previously after each window slide, the new window was taken from the unmodified audio.
    Now a window/2 size slice is where the subtraction happens. After all notes are removed, the first w/2 is discarded, but the end of the subtraction that happend in the second half of the window is preserved. The new half-window slices is than attached to the end.

--- main_entry.py
+++ main_entry.py
@@ -20,14 +20,13 @@ from util_audio import audio_complete
 
 
 class Hyperparams:
-    def __init__(self, N=4096, sr=44100, H=None, window_size_note_time=None, window_th=None):
+    def __init__(self, N=4096, sr=44100, H=None, window_size_note_time=None):
         self.N = N
         self.sr = sr
         self.H = np.int(N / 4) if H is None else H
-        self.window_size_note_time = 3 if window_size_note_time is None else window_size_note_time  # in FS
-        self.window_th = 0.2 if window_th is None else window_th
+        self.window_size_note_time = 6 if window_size_note_time is None else window_size_note_time
         self.pitch_input_shape = 20
-        self.timing_input_shape = 215
+        self.timing_input_shape = 258
 
         self.kernel_sizes = [(32, 3), (32,3)]
         self.pool_sizes = [(5, 2), (5, 2)]
@@ -42,33 +41,34 @@ class Hyperparams:
         self.residual_layer_frequencies = [2]
 
 
-def relevant_notes(sequence, audio, offset, duration, duration_in_frames):
+def relevant_notes(sequence, offset, duration):
     notes_w, notes_target = sequence.get_notes(offset, offset + duration)
     minus = notes_w.start_first
 
     notes_w = notes_w.clone()
-    audio = audio.section(offset, None, duration_in_frames)
     print(offset, minus)
 
     notes_target = notes_target.clone()
     notes_target.shift(-offset)
     notes_w.shift(-offset + minus)
-    return audio, notes_target, notes_w
+    return notes_target, notes_w
 
 
 # noinspection PyShadowingNames
 def pre_train(path, sf_path, params):
     """ Prepare data, training and test"""
     dm = util_dataset.DataManager(path, sets=['training', 'test'], types=['midi'])
-    onset_detector = OnsetDetector(params)
-    onset_detector.plot_model(os.path.join(path,'model_meta','onset'+'.png'))
-    duration_detector = DurationDetector(params)
-    duration_detector.plot_model(os.path.join(path,'model_meta','duration'+'.png'))
-    pitch_classifier = PitchClassifier(params)
-    pitch_classifier.plot_model(os.path.join(path,'model_meta','pitch'+'.png'))
-    instrument_classifier = InstrumentClassifier(params)
-    instrument_classifier.plot_model(os.path.join(path,'model_meta','instrument'+'.png'))
+#    onset_detector = OnsetDetector(params)
+#    onset_detector.plot_model(os.path.join(path,'model_meta','onset'+'.png'))
+#    duration_detector = DurationDetector(params)
+#    duration_detector.plot_model(os.path.join(path,'model_meta','duration'+'.png'))
+#    pitch_classifier = PitchClassifier(params)
+#    pitch_classifier.plot_model(os.path.join(path,'model_meta','pitch'+'.png'))
+#    instrument_classifier = InstrumentClassifier(params)
+#    instrument_classifier.plot_model(os.path.join(path,'model_meta','instrument'+'.png'))
     frametime = params.H / params.sr
+    halfwindow_frames = int(params.timing_input_shape/2)
+    halfwindow_time = int(params.window_size_note_time/2)
 
     dm.set_set('training')
     for fn in dm:
@@ -76,58 +76,70 @@ def pre_train(path, sf_path, params):
 
         sheet = note_sequence()
 
-        DEBUG = 0
+        DEBUG = 1
         if DEBUG:
             audio_w_last = None
         offset = 0
 
+        if DEBUG:
+            print('Generating wav for midi')
         mid_wf = audio_complete(mid.render(sf_path), params.N - 2)
         # TODO - Remove drums - hopefully it can learn to ignore it though
         # TODO -  Add random instrument shifts
-        audio_w, notes_target, notes_w = relevant_notes(mid, mid_wf, offset, params.window_size_note_time,
-                                                        params.timing_input_shape)
+        
+        if DEBUG:
+            print('Creating first cut')
+            
+        audio_w = mid_wf.section(offset, None, params.timing_input_shape)
+        notes_target, notes_w = relevant_notes(mid, offset, 
+                                               params.window_size_note_time)
         while offset < mid.duration:
 
             if DEBUG:
                 fn_base = os.path.join(path, 'debug', os.path.split(fn[0])[-1][:-4] + str(DEBUG))
                 print(fn_base)
-                #               if audio_w is not audio_w_last:
-                #               audio_w.save(fn_base+'.flac')
-                #               audio_complete(notes_target.render(sf_path),params.N-2).save(fn_base + '_target.flac')# -- tested, this works as intended
-
-                #               audio_w_last = audio_w
-                #               notes_target.save(fn_base+'_target.mid')
-                #               notes_w.save(fn_base+'_inclusive.mid')
+                if audio_w is not audio_w_last or True: #only print when new section emerges
+                   audio_w.save(fn_base+'.flac')
+#                   audio_complete(notes_target.render(sf_path),params.N-2).save(fn_base + '_target.flac')# -- tested, this works as intended
+                
+                   audio_w_last = audio_w
+#                   notes_target.save(fn_base+'_target.mid')
+#                   notes_w.save(fn_base+'_inclusive.mid')
                 DEBUG += 1
 
             note_gold = notes_target.pop(lowest=True, threshold=frametime)
             # training
             if note_gold is not None:
-                print(offset, note_gold.start_time)
+                print('Offset/Note start time = {:.3f} / {:.3f}'.
+                      format(offset, note_gold.start_time))
+                
                 onset_gold = note_gold.start_time
                 duration_gold = note_gold.end_time - note_gold.start_time
                 pitch_gold = note_gold.pitch
                 instrument_gold = note_gold.program
             else:
-                onset_gold = params.window_size_note_time + offset
-            onset_s = onset_detector.detect(audio_w, onset_gold)
-            duration_s = duration_detector.detect(audio_w, duration_gold)
+                onset_gold = offset + halfwindow_time
+            
+#            onset_s = onset_detector.detect(audio_w, onset_gold)
+#            duration_s = duration_detector.detect(audio_w, duration_gold)
 
             # use correct value to move window
-            if onset_gold + duration_gold >= offset + params.window_size_note_time:
-                # TODO: Fine tune window shift -- need to subtract the end of
-                # the subtracted notes from subsequent windows, otherwise,
-                # they sound like they start there. Or remove overlapping notes
-
-                offset += params.window_size_note_time - params.window_th
-                audio_w, notes_target, notes_w = relevant_notes(mid, mid_wf, offset, params.window_size_note_time,
-                                                                params.timing_input_shape)
+            if onset_gold >= halfwindow_time:                
+                offset += halfwindow_time
+                
+                audio_w_new = mid_wf.section(offset+halfwindow_time,
+                                             None, halfwindow_frames)
+                audio_w.slice_power(halfwindow_frames, 2*halfwindow_frames)
+                audio_w.concat_power(audio_w_new)
+                
+                notes_target, notes_w = relevant_notes(mid, offset, 
+                                                       params.window_size_note_time)
                 continue
 
             audio_sw = audio_w.resize(onset_gold, duration_gold, params.pitch_input_shape)
 
-            pitch_s = pitch_classifier.classify(audio_sw, pitch_gold)
-            instrument_sw = instrument_classifier.classify(audio_sw, instrument_gold)
+#            pitch_s = pitch_classifier.classify(audio_sw, pitch_gold)
+#            instrument_sw = instrument_classifier.classify(audio_sw, instrument_gold)
 
             # subtract correct note for training:
             note_guessed = note_sequence()
@@ -137,16 +149,16 @@ def pre_train(path, sf_path, params):
 
             ac_note_guessed = audio_complete(note_guessed.render(sf_path), params.N - 2)
             if DEBUG:
-                print(onset_gold)
+#                print(onset_gold)
             #               ac_note_guessed.save(fn_base+'_guess.flac')
-            #               note_last = note_gold
+                note_last = note_gold
             audio_w.subtract(ac_note_guessed, offset=onset_gold)
 
-            onset_s = onset_gold
-            instrument_sw = instrument_gold
-            pitch_s = pitch_gold
-            duration_s = duration_gold
-            sheet.add_note(instrument_sw, instrument_sw, pitch_s, onset_s + offset, onset_s + offset + duration_s)
+#            onset_s = onset_gold
+#            instrument_sw = instrument_gold
+#            pitch_s = pitch_gold
+#            duration_s = duration_gold
+#            sheet.add_note(instrument_sw, instrument_sw, pitch_s, onset_s + offset, onset_s + offset + duration_s)
 
         fn_result = os.path.join(path, 'results', os.path.split(fn[0])[-1])
         sheet.save(fn_result)
@@ -157,7 +169,7 @@ if __name__ == '__main__':
     # Hyperparams
     path = './data/'
     sf_path = '/home/hesiris/Documents/Thesis/GM_soundfonts.sf2'
-    p = Hyperparams(window_size_note_time=5, window_th=1)
+    p = Hyperparams(window_size_note_time=6)
     p.path = path
 
     pre_train(path, sf_path, p)
